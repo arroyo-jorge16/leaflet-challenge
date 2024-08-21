f7e2bbb91591471b9440bd16f72935fd5c34cc4b
Jorge Arroyo <arroyo.jorge2017@gmail.com>
2024-08-21 06:47:33 -0500

    update

--- Step_1/static/js/logic.js
+++ Step_1/static/js/logic.js
@@ -1,7 +1,5 @@
-// We create the tile layer that will be the background of our map.
 console.log("Step 1 working");
 
-// We create the tile layer that will be the background of our map.
 let basemap = L.tileLayer(
   "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'",
   {
@@ -10,7 +8,6 @@ let basemap = L.tileLayer(
   });
 
 
-// We create the map object with options.
 let map = L.map("map", {
   center: [
     40.7, -94.5
@@ -18,15 +15,13 @@ let map = L.map("map", {
   zoom: 3
 });
 
-// Then we add our 'basemap' tile layer to the map.
+
 basemap.addTo(map);
 
-// Here we make an AJAX call that retrieves our earthquake geoJSON data.
+
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson").then(function (data) {
 
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. We pass the magnitude of the earthquake into two separate functions
-  // to calculate the color and radius.
+
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -39,7 +34,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
     };
   }
 
-  // This function determines the color of the marker based on the magnitude of the earthquake.
   function getColor(depth) {
     switch (true) {
       case depth > 90:
@@ -57,8 +51,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
     }
   }
 
-  // This function determines the radius of the earthquake marker based on its magnitude.
-  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
+
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -67,15 +60,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
     return magnitude * 4;
   }
 
-  // Here we add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
     pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng);
     },
-    // We set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
-    // We create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
     onEachFeature: function (feature, layer) {
       layer.bindPopup(
         "Magnitude: "
@@ -88,12 +77,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
     }
   }).addTo(map);
 
-  // Here we create a legend control object.
   let legend = L.control({
     position: "bottomright"
   });
 
-  // Then add all the details for the legend
   legend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
 
@@ -107,7 +94,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
       "#ea2c2c"
     ];
 
-    // Looping through our intervals to generate a label with a colored square for each interval.
     for (let i = 0; i < grades.length; i++) {
       div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
         + grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
@@ -115,6 +101,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
     return div;
   };
 
-  // Finally, we our legend to the map.
+
   legend.addTo(map);
 });
